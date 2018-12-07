fed0597ab78948943c717d4ad15e9a0207f927c3
phuongbkatp <32545917+phuongbkatp@users.noreply.github.com>
2018-12-08 03:08:20 +0700

    delete all
    delete all

--- app/src/main/java/com/appian/datafilter/GlideHelper.java
+++
@@ -1,8 +0,0 @@
-package com.appian.datafilter;
-
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.AppGlideModule;
-
-@GlideModule
-public final class GlideHelper extends AppGlideModule {
-}
